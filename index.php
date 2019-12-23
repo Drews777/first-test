a828968e05ae5ea976d593ed2f1995be63fb5983
Drews777 <isakovdrews@gmail.com>
2019-12-23 23:28:55 +0700

    release new files

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="js/jquery.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="css/style.css?ver=111111111112">
+    <title>test.ru</title>
+  </head>
+  <body>
+    <div class="section_banner">
+      <div class="section_header">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <img src="" alt="logo">
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <ul class="nav_menu">
+                <li class="menu_link"><a href="">О компании</a></li>
+                <li class="menu_link"><a href="">Услуги</a></li>
+                <li class="menu_link"><a href="">Действующим клиентам</a></li>
+                <li class="menu_link"><a href="">Новым клиентам</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="section_banner_title">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+              <div class="banner_content">
+                <h1 class="main_title">Цифровое <br />воплощение <br />вашего <br />воображения</h1>
+                <div class="another_title">Разработаем сайт<br />который вам и не снился</div>
+                <a href="" class="banner_button">Связаться с нами</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section_packets">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="packets_content">
+              <h2 class="packet_title">Тарифные планы</h2>
+              <p class="packet_description">Мы разработали 3 тарифных плана, которые подойдут как для больших, так и для неочень компаний. В каждом тарифном плане есть свои преимущества, прежде чем определиться какой подойдет вам, проанализируйте необходимость тех или иных инструментов в каждом пакете. При возникновении затруднений с выбор просто оставьте заявку в <a style="text-decoration: underline;" href="">соответствующей форме</a>, мы с вами свяжемся и поможем.</p>
+            </div>
+          </div>
+          <div class="packet_hover col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <a href="" class="packet_link">
+              <div class="packets_content">
+                <h3 class="packet_title">Эконом</h3>
+                <p class="packet_description">Lorem ipsum dolor sit amet, odio accusam vel an. Eu civibus ocurreret contentiones qui.<br/><br/>Ut sit sensibus similique rationibus, nec bonorum mentitum te. Usu assum essent eirmod an, quo in posse molestie vituperata. Te qui ubique melius fastidii.</p>
+              </div>
+            </a>
+          </div>
+          <div class="packet_hover col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <a href="" class="packet_link">
+              <div class="packets_content">
+                <h3 class="packet_title">Оптиум</h3>
+                <p class="packet_description">Lorem ipsum dolor sit amet, odio accusam vel an. Eu civibus ocurreret contentiones qui.<br/><br/>Ut sit sensibus similique rationibus, nec bonorum mentitum te. Usu assum essent eirmod an, quo in posse molestie vituperata. Te qui ubique melius fastidii.</p>
+              </div>
+            </a>
+          </div>
+          <div class="packet_hover col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <a href="" class="packet_link">
+              <div class="packets_content">
+                <h3 class="packet_title">Бизнес</h3>
+                <p class="packet_description">Lorem ipsum dolor sit amet, odio accusam vel an. Eu civibus ocurreret contentiones qui.<br/><br/>Ut sit sensibus similique rationibus, nec bonorum mentitum te. Usu assum essent eirmod an, quo in posse molestie vituperata. Te qui ubique melius fastidii.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section_slider owl-carousel">
+      <div class="slide1">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Бесплатное<br/>продвижение<br/>в  <img src="/img/yandex.png" alt=""> *
+              <p>* При заказе тарифа бизнес</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="slide1">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Бесплатное<br/>продвижение<br/>в  <img src="/img/yandex.png" alt=""> *
+              <p>* При заказе тарифа бизнес</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="slide1">
+        <div class="container">
+          <div class=" row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">Бесплатное<br/>продвижение<br/>в  <img src="/img/yandex.png" alt=""> *
+              <p>* При заказе тарифа бизнес</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section2_slider">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+            <img src="/img/slider_photo11.jpg" alt="" class="slider_image">
+            <div class="slider_title">Сайт техники Apple</div>
+            <div class="slider_description">Кейс о том как мы взялись за создание сайта для продажи техники Apple и увеличили продажи в 3,5 раза, обогнав всех конкурентов в городе...</div>
+            <a href="" class="slider_link">Подробнее</a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+          <img src="/img/slider_photo2.jpg" alt="" class="slider_image">
+            <div class="slider_title">Библиотечный веб-ресурс</div>
+            <div class="slider_description">Приобщить людей к чтению задача не из простых. Но когда за работу беруться профессионалы, все встает на свои места.</div>
+            <a href="" class="slider_link">Подробнее</a>
+          </div>
+          <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+          <img src="/img/slider_photo3.jpg" alt="" class="slider_image">
+            <div class="slider_title">Аналитика вашего сайта</div>
+            <div class="slider_description">Ваш сайт не приносит вам желаемый доход, мы проведем качественный аудит вашего сайта и укажем на все слабые места.</div>
+            <a href="" class="slider_link">Подробнее</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section_form">
+      <div class="container">
+        
+          <h1 class="form_title">Оставить заявку</h1>
+          <div class="row">
+          <form action="" method="POST">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div>
+                <label for="">Имя</label>
+                <input type="text">
+              </div>
+              <div>
+                <label for="">Отчество</label>
+                <input type="text">
+              </div>
+              <div>
+                <label for="">Фамилия</label>
+                <input type="text">
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+              <div>
+                <label for="">Email</label>
+                <input type="text">
+              </div>
+              <div>
+                <label for="">Контактный телефон</label>
+                <input type="text">
+              </div>
+              <div>
+                <label for="">Интересующий вопрос</label>
+                <input type="text">
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="section_footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_title">О компании</div>
+            <a href="">О нас</a>
+            <a href="">Vk</a>
+            <a href="">Instagram</a>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_title">Услуги</div>
+            <a href="">Разработка сайтов</a>
+            <a href="">Дизайн и проектирование</a>
+            <a href="">Продвижение в интернете</a>
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_title">Контакты</div>
+            <p>+7 (495) 123-45-67</p>
+            <p>+7 (495) 123-45-67</p>
+            <p>itcompany@top1.com</p>                              
+          </div>
+          <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+            <div class="footer_title">Политика конфиденциальности</div>
+            <a href="">Читать</a>          
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section_rights">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10 col-md-10 col-sm-6 col-xs-12">
+            <p>Данный интернет-сайт носит исключительно информационный характер и ни при каких условиях не является публичной офертой, определяемой положениями Статьи 437(2) Гражданского кодекса Российской Федерации.</p>
+            <p>Для получения подробной информации о наличии и стоимости указанных товаров и (или) услуг, пожалуйста, обращайтесь по телефонам, указанным в контактах.</p>
+          </div>
+          <div class="col-lg-2 col-md-2 col-sm-6 col-xs-12">
+            <img src="" alt="logo">
+          </div>
+        </div>
+      </div>
+    </div>
+    <script>
+    $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        1000:{
+            items:1
+        },
+        1600:{
+            items:1
+        }
+    }
+  })
+  </script>
+  </body>
+  
+</html>
